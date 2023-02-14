b195e9fc76f4d87ff25c873796f8fe7c0b6d58b8
VladT8 <tishuk.vladzislau@gmail.com>
2023-02-15 01:00:41 +0300

    vremenno zakomentil header

--- axit-project/js/index.js
+++ axit-project/js/index.js
@@ -10,3 +10,9 @@ window.addEventListener('click', onWindowClick, false)
 window.onload = function () {
     links[0].click();
 };
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger , .header__menu').toggleClass('active');
+    });
+});
