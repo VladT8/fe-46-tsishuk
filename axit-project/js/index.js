dbe3d71e0f544a00f7d06da4abe801fb9733bcb2
VladT8 <tishuk.vladzislau@gmail.com>
2023-02-15 23:37:01 +0300

    add slider in reviews

--- axit-project/js/index.js
+++ axit-project/js/index.js
@@ -10,3 +10,20 @@ window.addEventListener('click', onWindowClick, false)
 window.onload = function () {
     links[0].click();
 };
+
+const imgContainer = document.querySelector('.customer__inner')
+const dotsContainer = document.querySelector('.nav__dots')
+
+
+document.getElementById('dot1').addEventListener('click', ()=>{
+    imgContainer.className = 'customer__inner first-active'
+    dotsContainer.className = 'nav__dots first-current'
+})
+document.getElementById('dot2').addEventListener('click', ()=>{
+    imgContainer.className = 'customer__inner second-active'
+    dotsContainer.className = 'nav__dots second-current'
+})
+document.getElementById('dot3').addEventListener('click', ()=>{
+    imgContainer.className = 'customer__inner third-active'
+    dotsContainer.className = 'nav__dots third-current'
+})
