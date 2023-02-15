b0680a81e337a8813d63a7133afbbdf4d413b610
VladT8 <tishuk.vladzislau@gmail.com>
2023-02-15 17:08:36 +0300

    remake header and add burger

--- axit-project/js/index.js
+++ axit-project/js/index.js
@@ -10,9 +10,3 @@ window.addEventListener('click', onWindowClick, false)
 window.onload = function () {
     links[0].click();
 };
-
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger , .header__menu').toggleClass('active');
-    });
-});
